586ace6ea9ca2555f993bb39eb036de8b0af33ae
Marina <dubovik.m14@gmail.com>
2017-12-26 00:36:42 +0200

    add task03

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer  = require('multer');
 const uuidv4 = require('uuid/v4');
+const sharp = require('sharp');
 
 const storage = multer.diskStorage({
   destination: './uploads/',
@@ -16,12 +17,27 @@ const pdfStorage = multer.diskStorage({
     }
 });
 
+const imageStorage = multer.diskStorage({
+    destination: './uploads/images/',
+    filename: function (req, file, cb) {
+        cb(null, uuidv4() + "-master" + file.originalname.slice(-4));
+    }
+});
+
 const upload = multer({ storage: storage });
 
 const pdfUpload = multer({
     storage: pdfStorage,
     fileFilter: function fileFilter(req, file, cb) {
         file.originalname.toUpperCase().endsWith(".PDF") ? cb(null, true) : cb(null, false);
+    }
+});
+
+const imageUpload = multer({
+    storage: imageStorage,
+    fileFilter: function fileFilter(req, file, cb) {
+        file.originalname.toUpperCase().endsWith(".JPG") || file.originalname.toUpperCase().endsWith(".PNG")
+            ? cb(null, true) : cb(null, false);
     }
 });
 
@@ -36,6 +52,19 @@ app.post('/pdf', pdfUpload.array('files', 3), (req, res, next) => {
     res.json({files: files.map((file) => file = file.filename)});
 });
 
+app.post('/images', imageUpload.single('image'), async (req, res, next) => {
+    let regexp = /-master\.[^\.]+$/;
+    let filenames = [
+        req.file.filename.replace(regexp, "-preview" + req.file.filename.slice(-4)),
+        req.file.filename.replace(regexp, "-thumbnail" + req.file.filename.slice(-4)),
+        req.file.filename
+    ];
+    await sharp('./uploads/images/' + req.file.filename).resize(800, 600).toFile('./uploads/images/' + filenames[0]);
+    await sharp('./uploads/images/' + req.file.filename).resize(300, 180).toFile('./uploads/images/' + filenames[1]);
+    res.json(filenames);
+});
+
 app.use(express.static('public'));
+app.use('/images', express.static('uploads/images'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
