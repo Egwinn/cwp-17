2380b4261756b779cfb6f83191d7b18614246cb7
Marina <dubovik.m14@gmail.com>
2017-12-26 00:10:42 +0200

    add task02

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer  = require('multer');
+const uuidv4 = require('uuid/v4');
 
 const storage = multer.diskStorage({
   destination: './uploads/',
@@ -8,12 +9,31 @@ const storage = multer.diskStorage({
   }
 });
 
+const pdfStorage = multer.diskStorage({
+    destination: './uploads/pdf/',
+    filename: function (req, file, cb) {
+        cb(null, uuidv4() + ".pdf");
+    }
+});
+
 const upload = multer({ storage: storage });
+
+const pdfUpload = multer({
+    storage: pdfStorage,
+    fileFilter: function fileFilter(req, file, cb) {
+        file.originalname.toUpperCase().endsWith(".PDF") ? cb(null, true) : cb(null, false);
+    }
+});
 
 const app = express();
 
 app.post('/upload', upload.single('file'), (req, res, next) => {
   res.json({ succeed: true });
+});
+
+app.post('/pdf', pdfUpload.array('files', 3), (req, res, next) => {
+    let files = req.files;
+    res.json({files: files.map((file) => file = file.filename)});
 });
 
 app.use(express.static('public'));
